9932e2e4674857112dbb76d17749a88fcbd40013
lanyue <2586851139@qq.com>
2023-11-27 20:43:32 +0800

    LF

---
+++ workflow/config/envs/humann3.post-deploy.sh
@@ -0,0 +1,40 @@
+root_path=$(dirname `dirname \`dirname "$CONDA_PREFIX"_\``)
+config_path="${root_path}"/workflow/config
+env_name=$(basename $CONDA_PREFIX)
+
+# Specify the version of the metaphlan3 database to match the version of humann3 (cannot use the latest version)
+metaphlan3_db_version="v30_CHOCOPhlAn_201901"
+echo "metaphlan3_db_version: ${metaphlan3_db_version}" >> "${root_path}"/logs/env.log
+echo $metaphlan3_db_version > "${CONDA_PREFIX}"/envs/humann3/lib/python3.1/site-packages/metaphlan/metaphlan_databases/mpa_latest
+
+# Read the configuration
+db_root=$(awk -F ': ' '/db_root:/{print $2}' "${config_path}"/config.yaml)
+humann_chocophlan=$(awk -F ': ' '/humann_chocophlan:/{print $2}' "${config_path}"/config.yaml)
+humann_uniref=$(awk -F ': ' '/humann_uniref:/{print $2}' "${config_path}"/config.yaml)
+humann_utility_mapping=$(awk -F ': ' '/humann_utility_mapping:/{print $2}' "${config_path}"/config.yaml)
+
+{
+  echo "Make additional adjustments for the post-deployment of the Conda environment ${env_name} (humann3)"
+  echo db_root: "$db_root"
+  echo humann_chocophlan: "$humann_chocophlan"
+  echo humann_uniref: "$humann_uniref"
+  echo humann_utility_mapping: "$humann_utility_mapping"
+} >> "${root_path}"/logs/env.log
+
+# Check whether the kraken2 database already exists
+if [ -d "${db_root}"/"${humann_chocophlan}" ] && [ -d  "${db_root}"/"${humann_uniref}" ] && [ -d  "${db_root}"/"${humann_utility_mapping}" ];then
+  echo "Humann3 database already exists, location：${db_root}/$(basename $humann_uniref)" >> "${root_path}"/logs/env.log
+  humann_config --update database_folders nucleotide "${db_root}"/"${humann_chocophlan}"
+  humann_config --update database_folders protein "${db_root}"/"${humann_uniref}"
+  humann_config --update database_folders utility_mapping "${db_root}"/"${humann_utility_mapping}"
+else
+  echo "The Humann3 database doesn't exist. Use the following command to build it yourself:" >> "${root_path}"/logs/env.log
+  # download the humann3 database
+  echo "conda activate $CONDA_PREFIX && cd ${db_root} && humann_databases --download uniref uniref90_diamond ${humann_uniref} --update-config yes" >> "${root_path}"/logs/env.log
+  echo "conda activate $CONDA_PREFIX && cd ${db_root} && humann_databases --download utility_mapping full ${humann_uniref} --update-config yes" >> "${root_path}"/logs/env.log
+  echo "conda activate $CONDA_PREFIX && cd ${db_root} && humann_databases --download chocophlan full ${humann_chocophlan} --update-config yes" >> "${root_path}"/logs/env.log
+fi
+
+echo "When fisrt running humann3, metaphlan3 database will be downloaded automatically." >> "${root_path}"/logs/env.log
+echo "Which will take a long time, more then" >> "${root_path}"/logs/env.log
+echo "---------------------------" >> "${root_path}"/logs/env.log
---
+++ workflow/rules/ly_test.smk
@@ -0,0 +1,38 @@
+rule cat_reads:
+    input:
+        fq1=config["root"] + "/" + config["folder"]["rm_host"] + "/{sample}/{sample}_paired_1.fq",
+        fq2=config["root"] + "/" + config["folder"]["rm_host"] + "/{sample}/{sample}_paired_2.fq",
+        # host_fq1=config["root"] + "/" + config["folder"]["rm_host"] + "/{sample}/{sample}_host_1.fq",
+        # host_fq2=config["root"] + "/" + config["folder"]["rm_host"] + "/{sample}/{sample}_host_2.fq",
+    output:
+        all_fq=config["root"] + "/" + config["folder"]["humann"] + "/{sample}/{sample}_all.fq"
+    message:
+        "08 : Cat all reads together for humann3 ------------------------------------------"
+    threads:
+        12
+    params:
+        opt=config["root"] + "/" + config["folder"]["humann"] + "/{sample}/"
+    shell:
+        """
+        mkdir -p params.opt
+        cat {input.fq1} {input.fq2} > {output.all_fq}
+        """
+
+rule generate_humann3_report:
+    input:
+        all_fq=config["root"] + "/" + config["folder"]["humann"] + "/{sample}/{sample}_all.fq"
+    output:
+        res_dir=directory(config["root"] + "/" + config["folder"]["humann"] + "/{sample}/{sample}_res")
+    message:
+        "09 : Run humann3 to generate reads functional annotation -------------------------"
+    threads:
+        24
+    log:
+        config["root"] + "/" + config["folder"]["humann"] + "/{sample}/{sample}.log"
+    conda:
+        config["root"] + "/" + config["envs"] + "/" + "humann3.yaml"
+    shell:
+        """
+        humann --input {input.all_fq} --output {output.res_dir} --threads {threads} \
+         --verbose --remove-temp-output  > {log} 2>&1
+        """
